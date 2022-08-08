76751f8874ca83991a292b8a502886fe5a9398f2
pearz <1095971161@qq.com>
2022-08-08 10:18:07 +0800

    2022.8.8 10:18 pearz

---
+++ src/main/java/com/red/figureapi/controller/CustomerGroupController.java
@@ -0,0 +1,16 @@
+package com.red.figureapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author pearz
+ * @Email zhaihonghao317@163.com
+ * @Date 10:17 2022-08-08
+ */
+@RestController
+@RequestMapping("/customergroup")
+public class CustomerGroupController {
+
+}
